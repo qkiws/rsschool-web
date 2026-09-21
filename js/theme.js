e76a7e04ffcf4514ee6cc491cbaf22f4d33618ff
syon <romatretak700@gmail.com>
2026-09-21 21:54:19 +0300

    fix(theme.js): add event listiner on themeButton
    Clicking was not working due to Listiner wasnt setuped

--- js/theme.js
+++ js/theme.js
@@ -7,3 +7,15 @@ if (savedTheme === 'dark'){
 } else {
     themeButton.textContent = "🌙";
 }
+
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+
+    if (document.body.classList.contains("dark-theme")) {
+        localStorage.setItem("theme", "dark");
+        themeButton.textContent = "🌞";
+    } else {
+        localStorage.setItem("theme", "light");
+        themeButton.textContent = "🌙";
+    }
+});
